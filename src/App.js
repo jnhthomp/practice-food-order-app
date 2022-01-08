19342dfe87aa1fea04780738abb99af5d2925379
Jonah <jnh.thompson@gmail.com>
2022-01-08 18:36:00 -0500

    Created <Meals> component
    <Meals> outputs paragraph blurb (<MealsSummary>) and list of meals
    (<AvailableMeals>)
    Currently list of meals is not styled and only outputs the name of the
    meal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from 'react';
 import Header from './Components/Layout/Header.jsx';
+import Meals from './Components/Meals/Meals.jsx';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
