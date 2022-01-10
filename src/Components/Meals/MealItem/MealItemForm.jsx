dba790a82a26705f5d9d62413e3cd31f79ecab77
Jonah <jnh.thompson@gmail.com>
2022-01-10 12:45:18 -0500

    Updated to pass id to <Input>
    Previously <Input> had a hardcoded id which did not change between calls
    Now this is assigned dynamically by passing down the id for the meal

--- src/Components/Meals/MealItem/MealItemForm.jsx
+++ src/Components/Meals/MealItem/MealItemForm.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input.jsx';
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
   return (
     <form className={classes.form}>
-      <Input label="Amount" input={{
-        id: 'amount',
-        type: 'number',
-        min: '1',
-        max: '5',
-        step: '1',
-        defaultValue: '1'
-      
-      }} />
+      <Input 
+        label="Amount" 
+        input={{
+          id: 'amount_' + props.id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1'
+        }} 
+      />
       <button>+ Add</button>
     </form>
   )
