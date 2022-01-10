d0c2ec07e6d8b1910f75043354bc8b0fc2f88d50
Jonah <jnh.thompson@gmail.com>
2022-01-10 12:02:03 -0500

    Added custom input component for reusability

--- src/Components/Meals/MealItem/MealItem.jsx
+++ src/Components/Meals/MealItem/MealItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm.jsx';
 
 const MealItem = (props) => {
 
@@ -12,7 +13,9 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   )
 }
